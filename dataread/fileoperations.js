926223397a141e1dc8489ce62f90aaf900f5627d
Pritha Rai <pritharai873@gmail.com>
2024-07-25 23:09:39 +0530

    string version2

--- dataread/fileoperations.js
+++ dataread/fileoperations.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 
 console.log('starting to read');
 const buffer = fs.readFileSync('data.txt')
-console.log(buffer);
+console.log(buffer.toString());
 console.log('this is another operation being performed')
