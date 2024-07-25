393a9ae7e689258c088787ee7052f6577b84a7e6
Pritha Rai <pritharai873@gmail.com>
2024-07-25 23:13:33 +0530

    stringencode

--- dataread/fileoperations.js
+++ dataread/fileoperations.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 console.log('starting to read');
-const buffer = fs.readFileSync('data.txt')
-console.log(buffer.toString());
+// block the code
+const buffer = fs.readFileSync('data.txt', { encoding: 'utf8'})
+console.log(buffer);
 console.log('this is another operation being performed')
